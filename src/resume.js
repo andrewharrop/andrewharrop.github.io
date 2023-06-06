63b692699044481f1c621ebdc0fbaa92e10f58a3
andrewharrop <andrewhar05@gmail.com>
2023-06-05 22:18:27 -0400

    h

--- src/resume.js
+++ src/resume.js
@@ -23,8 +23,19 @@ function Resume() {
     return (
         <div>
             Hi, I am Andrew
-            
-
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                    One of three columns
+                    </div>
+                    <div class="col-sm">
+                    One of three columns
+                    </div>
+                    <div class="col-sm">
+                    One of three columns
+                    </div>
+                </div>
+                </div>
         </div>
     )
 }
