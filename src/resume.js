600b597ae761bf5b08d263a926bdc83247299403
andrewharrop <andrewhar05@gmail.com>
2023-06-06 10:57:51 -0400

    g

--- src/resume.js
+++ src/resume.js
@@ -6,6 +6,7 @@ import css3 from './images/css3.svg'
 import docker from './images/docker.svg'
 import git from './images/git.svg'
 import github_image from './images/github.svg'
+import andrew_image from './images/Picture1.png'
 import html5 from './images/html5.svg'
 import java from './images/java.svg'
 import javascript from './images/javascript.svg'
@@ -24,12 +25,15 @@ function Resume() {
         <div>
             <div class="container">
                 <div class="row">
-                    <h2>Hi, my name is Andrew Harrop!</h2>
+                <div class="p-5"></div>
+
+                    <h2 class="d-flex justify-content-center">Hi, my name is Andrew Harrop!</h2>
+                    <div class="p-5"></div>
                     <div class="col-sm">
                     Currently, I am a Software Engineer at TD Bank. In my position, I have focused on backend functions, specializing in handling vast volumes of batch and real-time banking and financial data. My practical expertise lies in utilizing technologies such as Java, Spring, Git, Linux, and a range of supporting tools related to testing, automation, business requests, agile methodologies, and big data analytics.
                     </div>
                     <div class="col-sm">
-                    Image
+                        <img class="profile-img" src={andrew_image} alt="andrew" />
                     </div>
                     <div class="col-sm">
                     <strong>Education:</strong> Bachelor of Engineering, Software Engineering, GPA:3.9/4.0, <a href="https://www.uwo.ca/">Western University</a> 
@@ -40,7 +44,9 @@ function Resume() {
                     <br/>
                     <a href="https://github.com/andrewharrop">GitHub</a>
                     </div>
+                    
                 </div>
+                
                 </div>
         </div>
     )
